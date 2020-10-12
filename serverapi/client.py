cb5b0a5bb3577d43b9fcd960d3369f6148b1595b
Lawrence <lawrenceais@gmail.com>
2020-10-13 00:06:34 +0800

    server for computer vision

--- serverapi/client.py
+++ serverapi/client.py
@@ -9,5 +9,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/tim")
+#BASE = "http://168.63.239.113:5000/"
+#BASE = "http://168.63.239.113/"
+response = requests.get(BASE + "helloworld/bill")
 print(response.json())
